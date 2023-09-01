91092428d89ca9c62d792e63a448fe65647b83aa
ygouguasse <ygouguasse@gmail.com>
2023-09-01 14:26:51 -0400

    initiali

--- newProjetTestGit/Form1.cs
+++ newProjetTestGit/Form1.cs
@@ -6,5 +6,10 @@ namespace newProjetTestGit
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("test");
+        }
     }
 }
